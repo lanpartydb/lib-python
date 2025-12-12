d5b8475cdb1ffed6965d92b819fdb82122020d2d
Jochen Kupperschmidt <homework@nwsnet.de>
2025-12-12 16:59:27 +0100

    Split serialization tests

--- tests/serialization/test_serialize_series.py
+++ tests/serialization/test_serialize_series.py
@@ -8,40 +8,7 @@ from textwrap import dedent
 import pytest
 
 from lanpartydb.models import Series
-from lanpartydb.serialization import (
-    serialize_series_to_toml,
-    serialize_series_list_to_toml,
-)
-
-
-def test_serialize_series_list_to_toml():
-    series_list = [
-        Series(
-            slug='gammalan',
-            name='GammaLAN',
-            alternative_names=[],
-            country_codes=['ca', 'us'],
-        ),
-        Series(
-            slug='deltalan',
-            name='DeltaLAN',
-            alternative_names=['Δ LAN', 'Δέλτα LAN'],
-            country_codes=['au'],
-        ),
-    ]
-
-    assert serialize_series_list_to_toml(series_list) == dedent("""\
-            [[series]]
-            slug = "gammalan"
-            name = "GammaLAN"
-            country_codes = ["ca", "us"]
-
-            [[series]]
-            slug = "deltalan"
-            name = "DeltaLAN"
-            alternative_names = ["Δ LAN", "Δέλτα LAN"]
-            country_codes = ["au"]
-            """)
+from lanpartydb.serialization import serialize_series_to_toml
 
 
 @pytest.mark.parametrize(
---
+++ tests/serialization/test_serialize_series_list.py
@@ -0,0 +1,39 @@
+"""
+:Copyright: 2024-2025 Jochen Kupperschmidt
+:License: MIT
+"""
+
+from textwrap import dedent
+
+from lanpartydb.models import Series
+from lanpartydb.serialization import serialize_series_list_to_toml
+
+
+def test_serialize_series_list_to_toml():
+    series_list = [
+        Series(
+            slug='gammalan',
+            name='GammaLAN',
+            alternative_names=[],
+            country_codes=['ca', 'us'],
+        ),
+        Series(
+            slug='deltalan',
+            name='DeltaLAN',
+            alternative_names=['Δ LAN', 'Δέλτα LAN'],
+            country_codes=['au'],
+        ),
+    ]
+
+    assert serialize_series_list_to_toml(series_list) == dedent("""\
+            [[series]]
+            slug = "gammalan"
+            name = "GammaLAN"
+            country_codes = ["ca", "us"]
+
+            [[series]]
+            slug = "deltalan"
+            name = "DeltaLAN"
+            alternative_names = ["Δ LAN", "Δέλτα LAN"]
+            country_codes = ["au"]
+            """)
